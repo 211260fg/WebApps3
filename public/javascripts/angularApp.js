aeae076b3369d34a7cc5db246b5dd8139d82f5b3
211260fg <florian_goeteyn@hotmail.com>
2016-12-03 17:59:50 +0100

    posts service update om posts weer te geven

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -1,8 +1,14 @@
 var app = angular.module('flapperNews', ['ui.router']);
 
-app.factory('posts', [function () {
+app.factory('posts', ['$http', function ($http) {
     var o = {
         posts: []
+    };
+
+    o.getAll = function() {
+        return $http.get('/posts').success(function(data){
+            angular.copy(data, o.posts);
+        });
     };
     return o;
 }]);
@@ -16,7 +22,12 @@ app.config([
             .state('home', {
                 url: '/home',
                 templateUrl: '/home.html',
-                controller: 'MainCtrl'
+                controller: 'MainCtrl',
+                resolve: {
+                    postPromise: ['posts', function(posts){
+                        return posts.getAll();
+                    }]
+                }
             });
 
         $stateProvider
