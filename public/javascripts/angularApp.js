d61a5e172f66a9252e8a6b91b0d465219189ad38
211260fg <florian_goeteyn@hotmail.com>
2016-12-03 17:54:11 +0100

    comments & upvotes

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -39,15 +39,15 @@ app.controller('MainCtrl', [
 
         //TODO switch posts
 
-        $scope.posts = [
+        /*$scope.posts = [
             {title: 'post 1', upvotes: 5},
             {title: 'post 2', upvotes: 2},
             {title: 'post 3', upvotes: 15},
             {title: 'post 4', upvotes: 9},
             {title: 'post 5', upvotes: 4}
-        ];
+        ];*/
 
-        //$scope.posts = posts.posts;
+        $scope.posts = posts.posts;
 
         $scope.addPost = function () {
             if (!$scope.title || $scope.title === '') {
--- routes/posts.js
+++ routes/posts.js
@@ -23,8 +23,45 @@ router.post('/posts', function(req, res, next) {
 });
 
 router.get('/posts/:post', function(req, res) {
-    res.json(req.post);
+    req.post.populate('comments', function(err, post) {
+        if (err) { return next(err); }
+
+        res.json(post);
+    });
 });
+
+router.put('/posts/:post/upvote', function(req, res, next) {
+    req.post.upvote(function(err, post){
+        if (err) { return next(err); }
+
+        res.json(post);
+    });
+});
+
+router.post('/posts/:post/comments', function(req, res, next) {
+    var comment = new Comment(req.body);
+    comment.post = req.post;
+
+    comment.save(function(err, comment){
+        if(err){ return next(err); }
+
+        req.post.comments.push(comment);
+        req.post.save(function(err, post) {
+            if(err){ return next(err); }
+
+            res.json(comment);
+        });
+    });
+});
+
+router.put('/posts/:post/comments/:comment/upvote', function(req, res, next) {
+    req.comment.upvote(function(err, comment){
+        if (err) { return next(err); }
+
+        res.json(comment);
+    });
+});
+
 
 router.param('post', function(req, res, next, id) {
     var query = Post.findById(id);
@@ -37,5 +74,18 @@ router.param('post', function(req, res, next, id) {
         return next();
     });
 });
+
+router.param('comment', function(req, res, next, id) {
+    var query = Comment.findById(id);
+
+    query.exec(function (err, comment){
+        if (err) { return next(err); }
+        if (!comment) { return next(new Error('can\'t find comment')); }
+
+        req.comment = comment;
+        return next();
+    });
+});
+
 
 module.exports = router;
