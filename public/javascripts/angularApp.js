9859853d2a1aec9075e2e681ee06d0492d2ba749
211260fg <florian_goeteyn@hotmail.com>
2016-12-03 18:23:41 +0100

    angular comments + upvotes uitwerking

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -10,6 +10,37 @@ app.factory('posts', ['$http', function ($http) {
             angular.copy(data, o.posts);
         });
     };
+
+    o.create = function(post) {
+        return $http.post('/posts', post).success(function(data){
+            o.posts.push(data);
+        });
+    };
+
+    o.upvote = function(post) {
+        return $http.put('/posts/' + post._id + '/upvote')
+            .success(function(data){
+                post.upvotes += 1;
+            });
+    };
+
+    o.get = function(id) {
+        return $http.get('/posts/' + id).then(function(res){
+            return res.data;
+        });
+    };
+
+    o.addComment = function(id, comment) {
+        return $http.post('/posts/' + id + '/comments', comment);
+    };
+
+    o.upvoteComment = function(post, comment) {
+        return $http.put('/posts/' + post._id + '/comments/'+ comment._id + '/upvote')
+            .success(function(data){
+                comment.upvotes += 1;
+            });
+    };
+
     return o;
 }]);
 
@@ -34,52 +65,60 @@ app.config([
             .state('posts', {
                 url: '/posts/{id}',
                 templateUrl: '/posts.html',
-                controller: 'PostsCtrl'
+                controller: 'PostsCtrl',
+                resolve: {
+                    post: ['$stateParams', 'posts', function($stateParams, posts) {
+                        return posts.get($stateParams.id);
+                    }]
+                }
             });
 
         $urlRouterProvider.otherwise('home');
     }]);
 
+//TODO scope.post uit commentaar
 app.controller('MainCtrl', [
     '$scope',
     'posts',
-    function ($scope, posts) {
+    //'post',
+    function ($scope, posts, post) {
 
 
         $scope.indextitle = 'Flapper News';
 
-        //TODO switch posts
-
-        /*$scope.posts = [
-            {title: 'post 1', upvotes: 5},
-            {title: 'post 2', upvotes: 2},
-            {title: 'post 3', upvotes: 15},
-            {title: 'post 4', upvotes: 9},
-            {title: 'post 5', upvotes: 4}
-        ];*/
-
         $scope.posts = posts.posts;
 
-        $scope.addPost = function () {
-            if (!$scope.title || $scope.title === '') {
-                return;
-            }
-            $scope.posts.push({
+        //$scope.post = post;
+
+        $scope.addPost = function(){
+            if(!$scope.title || $scope.title === '') { return; }
+            posts.create({
                 title: $scope.title,
                 link: $scope.link,
-                upvotes: 0,
-                comments: [
-                    {author: 'Joe', body: 'Cool post!', upvotes: 0},
-                    {author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
-                ]
             });
             $scope.title = '';
             $scope.link = '';
         };
 
-        $scope.incrementUpvotes = function (post) {
-            post.upvotes += 1;
+        $scope.incrementUpvotes = function(post) {
+            posts.upvote(post);
         };
+
+        $scope.addComment = function(){
+            if($scope.body === '') { return; }
+            posts.addComment(post._id, {
+                body: $scope.body,
+                author: 'user',
+            }).success(function(comment) {
+                $scope.post.comments.push(comment);
+            });
+            $scope.body = '';
+        };
+
+        //TODO upvote comment
+        /*$scope.incrementUpvotes = function(comment){
+            posts.upvoteComment(post, comment);
+        };*/
 
     }]);
 
